a582ab15b4c6feb723aa0fd293e33536318174be
Lauro <lauropimentel12@gmail.com>
2024-06-07 09:42:42 -0300

    Initial Commit

---
+++ AirBnB/AirBnBApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnBApp.swift
+//  AirBnB
+//
+//  Created by Lauro Pimentel on 07/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AirBnBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
