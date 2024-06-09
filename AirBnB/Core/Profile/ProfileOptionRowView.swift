79d10754672c93b39ed2699da5c30957dcd98dfa
Lauro <lauropimentel12@gmail.com>
2024-06-09 18:23:33 -0300

    profile view finished

---
+++ AirBnB/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileOptionRowView.swift
+//  AirBnB
+//
+//  Created by Lauro Pimentel on 09/06/24.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    let imageName:String
+    let title: String
+    var body: some View {
+        VStack{
+            HStack{
+                Image(systemName: imageName)
+                Text(title).font(.subheadline)
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "gear", title: "Settings")
+}
