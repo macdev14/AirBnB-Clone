b47361858d445d305f121a7f64fec0abf7095a3d
Lauro <lauropimentel12@gmail.com>
2024-06-09 22:46:19 -0300

    Listing Model

--- AirBnB/Core/Listings/Model/Listing.swift
+++ AirBnB/Core/Listings/Model/Listing.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Listing : Identifiable, Codable {
+    let id: String
+    let ownerUid : String
+    let ownerName : String
+    let ownerImageURL : String
+    let numberOfBedrooms: Int
+    let numberOfBathrooms: Int
+    let numberOfGuests: Int
+    let numberOfBeds: Int
+    var pricePerNight: Int
+    let latitude: Double
+    let longitude: Double
+    let address : String
+    let city : String
+    let state : String
+    let title : String
+    let rating : Double
+}
